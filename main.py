7ef1308608519b2e34753891f8e077b54ecabc04
kgpakshita <100424526+kgpakshita@users.noreply.github.com>
2024-03-30 22:22:04 +0530

    url loader changed main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+# Name: Akshita Jain
 import os
-import streamlit as st
 import pickle
 import time
+import streamlit as st
 from langchain import OpenAI
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 from langchain.document_loaders import UnstructuredURLLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
-from dotenv import load_dotenv
-load_dotenv()  # take environment variables from .env (especially openai api key)
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-st.title("RockyBot: News Research Tool 📈")
+from dotenv import load_dotenv
+load_dotenv()  # works same as .env
+
+st.title("Equity Research Analyzer Chatbot 📈")
 st.sidebar.title("News Article URLs")
 
 urls = []
@@ -31,19 +34,32 @@ if process_url_clicked:
     loader = UnstructuredURLLoader(urls=urls)
     main_placeholder.text("Data Loading...Started...✅✅✅")
     data = loader.load()
+
+    # text_splitter = RecursiveharacterTextSplitter(
+    #     separators=['\n', '.\n', '.', ','],
+    #     chunk_size=2000
+    # )
+
+
     # split data
     text_splitter = RecursiveCharacterTextSplitter(
         separators=['\n\n', '\n', '.', ','],
-        chunk_size=1000
+        chunk_size=2000
     )
     main_placeholder.text("Text Splitter...Started...✅✅✅")
     docs = text_splitter.split_documents(data)
+
+
     # create embeddings and save it to FAISS index
     embeddings = OpenAIEmbeddings()
     vectorstore_openai = FAISS.from_documents(docs, embeddings)
     main_placeholder.text("Embedding Vector Started Building...✅✅✅")
-    time.sleep(2)
+    time.sleep(1)
 
+
+
+
+#------------------------------------------------------------------------------------------------------------------------
     # Save the FAISS index to a pickle file
     with open(file_path, "wb") as f:
         pickle.dump(vectorstore_openai, f)
@@ -55,15 +71,15 @@ if query:
             vectorstore = pickle.load(f)
             chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
             result = chain({"question": query}, return_only_outputs=True)
-            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+# ----------------------------- result will be a dictionary of this format --> {"answer": "", "sources": [] }--------------------------------------------------
             st.header("Answer")
             st.write(result["answer"])
 
-            # Display sources, if available
+#----------------------------------Display sources, if available--------------------------------------------
             sources = result.get("sources", "")
             if sources:
                 st.subheader("Sources:")
-                sources_list = sources.split("\n")  # Split the sources by newline
+                sources_list = sources.split("\n")  
                 for source in sources_list:
                     st.write(source)
 
